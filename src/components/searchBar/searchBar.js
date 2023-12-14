52af5c408d2ca613d504dc80fe3fc2af1e2aca88
Joao Bertholo <joao.bertholo@gmail.com>
2023-12-14 17:04:39 +1100

    working <li> and submit button returning on console

--- src/components/searchBar/searchBar.js
+++ src/components/searchBar/searchBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import foodVideo from '../media/foodVideo.mp4';
 import styles from './SearchBar.module.css';
 
@@ -12,10 +12,36 @@ const sortByOptions = {
 
 function SearchBar() {
 
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchLocation, setSearchLocation] = useState('');
+    const [sortBy, setSortBy] = useState('best_match');
+
+    const handleSearchTerm = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const handleSearchLocation = (event) => {
+        setSearchLocation(event.target.value);
+    }
+
+    const handleSortBy = (sortByOption) => {
+        setSortBy(sortByOption);
+    }
+
+    const handleSubmitButton = (event) => {
+        event.preventDefault();
+        
+        console.log(`searching Yelp with ${searchTerm}, ${searchLocation}, ${sortBy}`)
+    }
+    
+
     const renderSortByOptions = () => {
         return Object.keys(sortByOptions).map((option) => {
-            let sortByOptionValue = option[sortByOptions];
-            return <li key={sortByOptionValue}>{option}</li>
+            let sortByOptionValue = sortByOptions[option];
+            return <li key={sortByOptionValue}
+            onClick={() => {handleSortBy(sortByOptionValue)}}
+            tabIndex='0'
+            >{option}</li>
         })
     }
 
@@ -25,10 +51,10 @@ function SearchBar() {
             <ul>{renderSortByOptions()}</ul>
         </div>
         <div className={styles.SearchBarFields}>
-            <input placeholder="Enter a restaurant"/>
-            <input placeholder="Enter a location"/>
+            <input value={searchTerm} onChange={handleSearchTerm} placeholder="Enter a restaurant"/>
+            <input value={searchLocation} onChange={handleSearchLocation} placeholder="Enter a location"/>
         </div>
-            <button className={styles.SearchBarSubmit} >Let's Go!</button>
+        <button className={styles.SearchBarSubmit} onClick={handleSubmitButton}>Let's Go!</button>
         <video src={foodVideo} autoPlay loop muted />
     </div>
     )
